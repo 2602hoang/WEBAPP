354b551de484a00104d85b9fe4802debf3eeb197
2602hoang <vuhuyhoangboj@gmail.com>
2023-12-03 20:42:24 +0700

    url

--- src/contexts/url.js
+++ src/contexts/url.js
@@ -2,7 +2,7 @@
 
 
 
-export const URL ='https://food-delivery-api-f1u2.onrender.com'
+export const URL ='http://172.104.181.251:3001'
 
 //export const URL ='http://192.168.1.182:3001/'
 // http://192.168.1.182:3001/
